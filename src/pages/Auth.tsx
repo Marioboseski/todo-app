95e401445d2a3dfcae3feac26b6cc933a383bbe9
Mario Boseski <crboseskimario@gmail.com>
2026-03-11 16:14:00 +0100

    Connect both LoginForm.tsx and RegisterForm.tsx in Auth.tsx with state

---
+++ src/pages/Auth.tsx
@@ -0,0 +1,21 @@
+import RegisterForm from "../components/auth/RegisterForm";
+import LoginForm from "../components/auth/LoginForm";
+import { useState } from "react";
+
+const Auth = () => {
+
+  const [ isLogin, setIsLogin ] = useState(true);
+  const toggleForm = () => {
+    setIsLogin(prev => !prev);
+  }
+
+  return (
+    <div>
+      {isLogin ? <LoginForm /> : <RegisterForm />}
+      <button onClick={toggleForm}>{isLogin ? "Don't have an account? Register"
+      : "Already have an account? Login"}</button>
+    </div>
+  );
+}
+
+export default Auth;
